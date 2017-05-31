1db2b48a6beed6aadee5617afed457d5c3c157ed
Mick van der Vegt <Mick van der Vegt>
2017-05-31 11:36:26 +0200

    Changed graphs

--- PriceProduct.m
+++ PriceProduct.m
@@ -33,6 +33,11 @@ LongCallStrikes = zeros(1, T/dT);
 LongCallAmounts = zeros(1, T/dT);
 ShortCallAmounts = zeros(1, T/dT);
 ShortCallStrikes = zeros(1, T/dT);
+Deltas = zeros(1, T/dT);
+Gammas = zeros(1, T/dT);
+Thetas = zeros(1, T/dT);
+Vegas = zeros(1, T/dT);
+Rhos = zeros(1, T/dT);
 
 for month_i = 1:(T/dT)
     day_i = ((month_i-1) * month_duration) + 1;
@@ -50,18 +55,48 @@ values = zeros(1, 10 * month_duration*payments_per_year);
 for day_i = 1:(size(StockPath, 2)-1)
 %     day_i
     S0 = StockPath(1, day_i);
-    values(1, day_i) = CalculateProductPrice(S0, LongCallStrikes, LongCallAmounts, ShortCallStrikes, ShortCallAmounts, BondFaceValues, T - (day_i/(month_duration*payments_per_year)));
+    
+     [present_value, Delta, Gamma, Theta, Vega, Rho] = CalculateProductPrice(S0, LongCallStrikes, LongCallAmounts, ShortCallStrikes, ShortCallAmounts, BondFaceValues, T - (day_i/(month_duration*payments_per_year)));
+     values(1, day_i) = present_value;
+     Deltas(1, day_i) = Delta;
+     Gammas(1, day_i) = Gamma;
+     Thetas(1, day_i) = Theta;
+     Vegas(1, day_i) = Vega;
+     Rhos(1, day_i) = Rho;
 end
 
-hold on
+figure
+subplot(4,1,1)       % add first plot in 2 x 1 grid
 plot(StockPath);
 yyaxis right
+xlabel('Time (in days)')
 plot(values);
+
+subplot(4,1,2)       % add second plot in 2 x 1 grid
+hold on
+plot(Deltas)
+plot(Gammas)
 hold off
+xlabel('Time (in days)')
+legend('Delta', 'Gamma');
+
+subplot(4,1,3)
+hold on
+plot(Vegas)
+plot(Rhos)
+hold off
+xlabel('Time (in days)')
+legend('Vega', 'Rho');
+
+subplot(4,1,4)
+plot(Thetas)
+xlabel('Time (in days)')
+legend('Theta');
+
 
 PayOff = values(1, 252*10) / sum(BondFaceValues)
 
-function present_value = CalculateProductPrice(S0, LongCallStrikes, LongCallAmounts, ShortCallStrikes, ShortCallAmounts, BondFaceValues, sim_T)
+function [present_value, Delta, Gamma, Theta, Vega, Rho] = CalculateProductPrice(S0, LongCallStrikes, LongCallAmounts, ShortCallStrikes, ShortCallAmounts, BondFaceValues, sim_T)
 %     T in year, till maturity
     global payments_per_year;
     global r_stock;
@@ -77,6 +112,11 @@ function present_value = CalculateProductPrice(S0, LongCallStrikes, LongCallAmou
     BondValue = 0;
 %     bsm_call(r, q, S0, K, T, sig)
 %     sim_T
+    Delta = 0;
+    Gamma = 0;
+    Theta = 0;
+    Vega = 0;
+    Rho = 0;
     for month_i = 1:current_month
         rfr_T = T - (month_i - 1) * dT;
         r = RiskFreeRateInterpolation(rfr_T);
@@ -88,16 +128,33 @@ function present_value = CalculateProductPrice(S0, LongCallStrikes, LongCallAmou
         
         LongValue =  LongAmount * bsm_call(r_stock, q, S0, LongStrike, sim_T, sig);
         ShortValue = ShortAmount * bsm_call(r_stock, q, S0, ShortStrike, sim_T, sig);
+        
+%         Can you just add these up?
+        [delta, gamma, theta, vega, rho] = bsm_call_greeks(r_stock, q, S0, LongStrike, sim_T, sig);
+        Delta = Delta + delta;
+        Gamma = Gamma + gamma;
+        Theta = Theta + theta;
+        Vega = Vega + vega;
+        Rho = Rho + rho;
+        
+%         Is the minus here correct?
+        [delta, gamma, theta, vega, rho] = bsm_call_greeks(r_stock, q, S0, ShortStrike, sim_T, sig);
+        Delta = Delta - delta;
+        Gamma = Gamma - gamma;
+        Theta = Theta - theta;
+        Vega = Vega - vega;
+        Rho = Rho - rho;
+        
+        
         BondValue = BondValue + BondFaceValues(1, month_i)*exp(-r * sim_T);
         OptionValue = OptionValue + (LongValue - ShortValue);
-        
     end
     present_value = OptionValue + BondValue;
 end
 
 
     
-    
+
 
 
 
